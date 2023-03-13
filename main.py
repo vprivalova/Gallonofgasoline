9a2cf6d9eb92f36fcd7cefc00626bef40893a63d
vprivalova <privalovaa.vv@gmail.com>
2023-03-13 09:30:52 +0700

    Corrections are made2.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ bbl_of_oil = volume_gal / 19.5
 carbon_dioxide = volume_gal * 20
 ethanol_volume = (volume_gal * 115000) / 75700
 cost_usd = volume_gal * 3
-avg_consume = 0.933
-population_nsk = 1621500
-population_rus = 145478097
-city_consume = avg_consume * population_nsk
-country_consume = avg_consume * population_rus * 365
+AVG_CONSUME = 0.933
+POPULATION_NSK = 1621500
+POPULATION_RUS = 145478097
+city_consume = AVG_CONSUME * POPULATION_NSK
+country_consume = AVG_CONSUME * POPULATION_RUS * 365
 
 print(ru.VOLUMEOUTPUT, round(volume_lit, 2))
 print(ru.OILBARRELS, round(bbl_of_oil, 2))
